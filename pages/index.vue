9b7b6a2b3ad05cff43680de598ed643947ebb849
ESHamza <essaadihamza4@outlook.com>
2022-07-04 20:03:28 +0100

    added home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,54 @@
-<template>
+<template >
+
+<div class="relative bg-black ">
+  <img 
+  class="h-screen w-full opacity-90 object-cover"
+   src="@/static/images/backimg.jpg" alt="random movies on a single image ">
+   
+
+  <div 
+    class="absolute w-full top-16 md:top-20   text-white p-8">
+       <h1 
+       class="text-white text-4xl md:font-black font-extrabold text-center"
+          >welcom to WMoviE </h1>
+      <p style="max-width:600px;" 
+        class="text-lg mx-auto sm:text-2xl text-center tracking-wide  font-medium mt-16 sm:mt-32 " >Watch movies online free full movie - Watch hd movies online and   download free hd movies online with WMoviE With our free streaming service, you can watch full movies online without registration.</p>
+    <div class="flex justify-around items-center my-16  sm:justify-center sm:space-x-10" >
+       <button> 
+          <NuxtLink 
+          style=""
+          class="rounded-lg bg-black px-4 py-2 border-2 m-2 mt-4 border-black text-white hover:bg-gray-900 hover:text-gray-100 hover:border-white duration-200"
+          to="/Movies"  > MOVIES  </NuxtLink>
+             </button> 
+      <button> 
+         <NuxtLink  
+         style=""
+         class="rounded-lg bg-black px-4 py-2 border-2 m-2 mt-4 border-black text-white hover:bg-gray-900 hover:text-gray-100 hover:border-white duration-200"
+         to="/Tvshows"  >TVSHOWS </NuxtLink>
+      </button>
+    </div>
+</div>
+  </div>
 
 </template>
 
+
 <script>
 export default {
-  name: 'IndexPage'
+   head() {
+      return {
+        title: "WMovie",
+      
+    }
 }
+}
+
 </script>
+<style>
+
+/* active links styling  */
+.nuxt-link-exact-active{
+   border-bottom: 1px solid  #fff;
+   transition: border .5s ease-in-out;
+}
+</style>
