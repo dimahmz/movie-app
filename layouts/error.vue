0b19803d31c66be42d4f2522469b28d264eb4756
ESHamza <essaadihamza4@outlook.com>
2022-08-01 07:14:26 +0100

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-screen text-white bg-black">
+    <h1>this path : {{ error.path }} doesn't exit in this website</h1>
+    <div class="h-screen grid place-items-center">
+      <NuxtLink
+        class="rounded-lg bg-black px-4 py-2 border-2 m-2 mt-4 border-black text-white hover:bg-gray-900 hover:text-gray-100 hover:border-white duration-200"
+        to="/"
+        >Home page
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+  layout: "noNavbar",
+  head() {
+    return {
+      title: "error",
+    };
+  },
+};
+</script>
